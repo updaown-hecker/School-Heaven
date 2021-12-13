b2735900d49cda5725b4256ae927f4c641c5d3f0
Blazer <51216880+BlaizerBrumo@users.noreply.github.com>
2021-12-13 03:15:32 +0000

    changed

--- index.js
+++ index.js
@@ -26,11 +26,6 @@ app.use('/', function(req, res) {
 //    else {res.send('<pre>Cannot GET ' + req.url + '</pre>')}
 //});
 
-app.use('/', function (req, res) {
-    if (req.url.startsWith(proxy.prefix)) {proxy.request(req,res)}
-    else {res.send('<pre>Cannot GET ' + req.url + '</pre>')}
-});
-
 app.listen(process.env.PORT || port, () => {
   console.log(`Fallen Angel is running at localhost:${port}`)
 });
